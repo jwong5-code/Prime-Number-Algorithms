7e69196c65c2dffd81a4deddc53ae5b643cb8da6
jwong5-code <81677353+jwong5-code@users.noreply.github.com>
2024-02-21 11:47:37 -0800

    Update Primetests.java

--- Primetests.java
+++ Primetests.java
@@ -1,5 +1,5 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+
 public class Primetests {
     public static boolean primetest1(int n) {
         boolean isprime = true;
